fcbf97bf05aa3ad9ec48a5d84be9eb294ac81a53
ruksana-nourin <ruksananourin@gmail.com>
2026-06-04 13:09:46 +0600

    project php templeting

---
+++ php-project/admin/config/base.php
@@ -0,0 +1,11 @@
+<?php
+//Local
+const BASE_URL = "http://localhost/php-project/";
+const BASE_URL_ADMIN = "http://localhost/php-project/admin/";
+
+//Hosting
+// const BASE_URL = "http://roxy.com/";
+// const BASE_URL_ADMIN = "http://roxy.com/admin/";
+
+
+?>
